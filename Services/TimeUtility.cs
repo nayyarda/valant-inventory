59c078ad8df844cd7da589a410c3ae247a80db4c
nayyarda <david.nayyar@gmail.com>
2016-04-03 15:25:57 -0400

    Added files via upload

---
+++ Services/TimeUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValantData
+{
+	//Allows unit tests to manipulate DateTime.Now 
+	public static class TimeUtility
+	{
+		private static bool overrideDateTimeNow = false;
+		private static DateTime modifiedNow;
+
+		public static DateTime Now
+		{
+			get{
+				if (overrideDateTimeNow) {
+					return modifiedNow;
+				} else {
+					return System.DateTime.Now;
+				}
+			}
+		}
+
+		public static void SetDateTimeNow(DateTime dateTimeNow)
+		{
+			overrideDateTimeNow = true;
+			modifiedNow = dateTimeNow;
+		}
+
+		public static void ResetDateTimeNow()
+		{
+			overrideDateTimeNow = false;
+			modifiedNow = DateTime.Now;
+		}
+	}
+}
+
