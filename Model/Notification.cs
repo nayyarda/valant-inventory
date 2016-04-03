1d3f61a362441f2719de2ae509847eae79a726df
nayyarda <david.nayyar@gmail.com>
2016-04-03 15:24:45 -0400

    Added files via upload

---
+++ Model/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValantData
+{
+	public class Notification
+	{
+		public Notification (Item item, TransactionType transactionType)
+		{
+			Item = item;
+			TransactionDate = TimeUtility.Now;
+			TransactionType = transactionType;
+		}
+
+		public Item Item {
+			get;
+			set;
+		}
+
+		public DateTime TransactionDate {
+			get;
+			set;
+		}
+
+		public TransactionType TransactionType {
+			get;
+			set;
+		}
+	}
+}
+
